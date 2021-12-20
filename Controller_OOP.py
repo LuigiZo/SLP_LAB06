aeb867c0318bbdf62089d9ca7bf5561f6e4d55a3
LuigiZo <92730811+LuigiZo@users.noreply.github.com>
2021-12-20 16:10:43 +0100

    Add files via upload

---
+++ Controller_OOP.py
@@ -0,0 +1,29 @@
+from Lab6.OOP_Lab import User
+from datetime import date, datetime
+
+class UserController:
+    def __init__(self):
+        self.users=[]
+    def create_user(self, name, last_name, address, gender, birth_date, start_date = date.today()):
+        u=User(name, last_name, address, gender, birth_date)
+        self.users.append(u)
+    def print_users(self):
+        for user in self.users:
+            print(user)
+
+m="male"
+f="female"
+gender= m or f
+#Point3
+add1=("80-142", "S.Paolo st.", 12)
+add2=("80-142", "Dworcowa st.", 21)
+add3=("80-141", "S.Gennaro st.", 8)
+add4=("80-140", "Ks. Ignacego", 78)
+add5=("80-143", "Jozefa st.", 91)
+uc=UserController()
+uc.create_user("Alex", "Cena", add1, m, date(2000, 2, 2))
+uc.create_user("Agata", "Happy", add2, f, date(1996, 3, 20))
+uc.create_user("John", "Rock", add3, m, date(2001, 11, 25))
+uc.create_user("Lia", "Fidat", add4, f, date(1993, 5, 12))
+uc.create_user("Marianna", "Macan", add5, f, date(1999, 8, 30))
+uc.print_users()
